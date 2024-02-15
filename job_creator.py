02f748bf58f08c5eee4d89a9c41b9c1f23d80c08
Zach <132305891+PuzzleZach@users.noreply.github.com>
2024-02-14 21:13:33 -0600

    c++ demo added

--- job_creator.py
+++ job_creator.py
@@ -1,7 +1,13 @@
 import concurrent.futures
 from types import FunctionType as function # Could just use Any or Callable
-from timer import Timer
+from timer import timer
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+timer.set_level(logging.DEBUG)
+
+
+@timer("create_jobs", unit='s')
 def create_jobs(func: function, items: list, workers: int) -> list:
   """
   This function creates a pool of workers to carry out a repetitive task on a list of items.
@@ -11,48 +17,37 @@ def create_jobs(func: function, items: list, workers: int) -> list:
   items -- A list of items we want to iterate over and call func against.
   workers -- The amount of workers we want to allocate to this thread pool.
   """
-
-  thread_results = []
-
   with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as exec:
     jobs = [exec.submit(func, item) for item in items]
 
     concurrent.futures.wait(jobs) # Wait for all the above jobs to finish processing.
 
-    for job in jobs:
-      thread_results.append(job.result())
+    return [job.result() for job in jobs]
 
-  return thread_results
 
 def expensive_operation(item: int) -> int:
   """
   An expensive operation that consumes time linearly.
   """
   i = 0
-  while i < 100000:
+  while i < 1000000:
     i += 1
   return item * -1
 
-def expensive_jobs(items: list) - > list:
+
+@timer("expensive_jobs", unit='s')
+def expensive_jobs(items: list) -> list:
   """
   Simple list generator to build the results from our expensive operation.
   """
-  new_items = [expensive_operation(item) for item in items)]
-  return new_items
+  return [expensive_operation(item) for item in items]
     
 
 if __name__ == "__main__":
-  # If we run this from the command line, perform our test.
-  positive_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  # If we run from the command line, perform our test.
+  positive_numbers = [x for x in range(10000)]
 
-  tracking = Timer()
-  tracking.start()
-  negative_numbers_1 = expensive_jobs(positive_numbers)
-  tracking.stop()
-  
-  tracking2 = Timer()
-  tracking2.start()
-  negative_numbers_1 = create_jobs(expensive_operation(), positive_numbers)
-  tracking2.stop()
+  create_jobs(expensive_operation, positive_numbers, 30)
+  expensive_jobs(positive_numbers)
 
   # We should see a time difference between the 2.
